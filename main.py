42151a29107e0dfdedce9ed18640befadb33a9a6
SummerAnan <107442849+summeranan1@users.noreply.github.com>
2022-10-06 06:32:44 +0000

    update

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ if len(word) > 4:
   print("Too much letters!")
 elif len(word) < 5:
   if word == word[3] + word[2] + word[1] + word[0]:
-    print("The words are the same")
+    print("The words are the same.")
   else:
-    print("The words are not the same")
+    print("The words are not the same.")
